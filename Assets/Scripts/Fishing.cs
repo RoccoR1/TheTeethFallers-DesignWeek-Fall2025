fb03315667f1a361f0247bac7576a3d3939aee2a
RoccoR1 <50677050+RoccoR1@users.noreply.github.com>
2025-10-29 14:44:58 -0400

    Changing to fishing mode fully functional
    Timer is properly implemented as well.

--- Assets/Scripts/Fishing.cs
+++ Assets/Scripts/Fishing.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class Fishing : MonoBehaviour
 {
+    public GameObject timerRing;
+    public GameObject fishingUI;
     public float scratchTimer;
     public bool hasScratchPrompted;
+    public bool isFishMode;
+    private Vector2 originalRingSize;
 
     private PlayerController controller;
     private PlayerInputController inputController;
@@ -14,22 +18,47 @@ public class Fishing : MonoBehaviour
         inputController = GetComponent<PlayerInputController>();
         scratchTimer = Random.Range(5, 10);
         hasScratchPrompted = false;
+        originalRingSize = timerRing.GetComponent<RectTransform>().sizeDelta;
+        isFishMode = false;
     }
 
     void Update()
     {
-        if (!inputController.isMoveMode)
+        if (isFishMode)
         {
             scratchTimer -= Time.deltaTime;
         }
         if (scratchTimer <= 3 && !hasScratchPrompted)
         {
+            timerRing.SetActive(true);
             hasScratchPrompted = true;
+        }
+        else if (scratchTimer <= 3)
+        {
+            timerRing.GetComponent<RectTransform>().sizeDelta = new Vector2(timerRing.GetComponent<RectTransform>().sizeDelta.x - Time.deltaTime, timerRing.GetComponent<RectTransform>().sizeDelta.y - Time.deltaTime);
         }
         if (scratchTimer <= 0)
         {
-            scratchTimer = Random.Range(5, 10);
-            hasScratchPrompted = false;
+            ResetTimer();
         }
+    }
+
+    public void ResetTimer()
+    {
+        scratchTimer = Random.Range(5, 10);
+        hasScratchPrompted = false;
+        timerRing.SetActive(false);
+        timerRing.GetComponent<RectTransform>().sizeDelta = originalRingSize;
+    }
+    public void StartFishing()
+    {
+        fishingUI.SetActive(true);
+        isFishMode = true;
+    }
+    public void StopFishing()
+    {
+        ResetTimer();
+        fishingUI.SetActive(false);
+        isFishMode = false;
     }
 }
--- Assets/Scripts/Player/PlayerInputController.cs
+++ Assets/Scripts/Player/PlayerInputController.cs
@@ -8,11 +8,13 @@ public class PlayerInputController : MonoBehaviour
     public Vector3 movementInputVector { get; private set; }
     public bool isMoveMode;
     private PlayerController playerController;
+    private Fishing fishing;
 
     private void Awake()
     {
         isMoveMode = true;
         playerController = GetComponent<PlayerController>();
+        fishing = GetComponent<Fishing>();
     }
     private void OnMove(InputValue inputValue)
     {
@@ -24,10 +26,22 @@ public class PlayerInputController : MonoBehaviour
     private void OnChangeMode(InputValue inputValue)
     {
         isMoveMode = !isMoveMode;
+        if (!fishing.isFishMode)
+        {
+            fishing.StartFishing();
+        }
+        else
+        {
+            fishing.StopFishing();
+        }
     }
     private void OnOpenLog()
     {
-        isMoveMode = !isMoveMode;
-        playerController.OpenLog();
+        // Only opens log if the user isn't fishing.
+        if (!fishing.isFishMode)
+        {
+            isMoveMode = !isMoveMode;
+            playerController.OpenLog();
+        }
     }
 }
