797c6ffe0b98a87ccc146144cfa462d6c665d78e
RoccoR1 <50677050+RoccoR1@users.noreply.github.com>
2025-10-30 20:19:20 -0400

    Made title screen
    Currently can change scene, but functionality to controls change will be implemented later (if possible).

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,22 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ChangeControls(int controlNum)
+    {
+        if (controlNum == 0)
+        {
+            //Make 0 be turntable rotation = movement forward/back
+        }
+        else if (controlNum == 1)
+        {
+            // Make 0 be turntable rotation = boat rotation
+        }
+    }
+}
